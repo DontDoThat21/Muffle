48aac9a80e180e0eea303cb287a107dfe543771b
Tylor Trub <tylort@uniqueusainc.com>
2024-05-10 14:52:11 -0400

    Add various DB references to Muffle.csproj and Muffle.Data.csproj. Added User model. Added DbContext (not using until SQL Server integrations way down the road). Added Data CSProj to SLN.

---
+++ Muffle.Data/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Muffle.Data.Data
+{
+    //public DbSet<example> example { get; set; }
+
+    public class AppDbContext : DbContext
+    {
+        //public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        //{
+            // not using context class today
+        //}
+    }
+}
