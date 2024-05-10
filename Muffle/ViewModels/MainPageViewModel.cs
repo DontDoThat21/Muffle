7df69968bae532ea2f968a35d30a244e950f160e
Tylor Trub <tylort@uniqueusainc.com>
2024-05-10 15:06:56 -0400

    Added MainPageViewModel, Model, and Server model.

--- Muffle/ViewModels/MainPageViewModel.cs
+++ Muffle/ViewModels/MainPageViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Muffle.Data.Models;
 
 namespace Muffle.ViewModels
 {
     public class MainPageViewModel
     {
-       
+        private readonly MainPageModel MainPage;
+        public ObservableCollection<Server> Servers { get; set; }
     }
 }
